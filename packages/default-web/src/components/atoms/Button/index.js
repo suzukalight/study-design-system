0b0570d09bec74e480c1a67bb17daf45ee33ec32
mkubara <masahiko_kubara@relationsgroup.co.jp>
2019-02-10 10:56:38 +0900

    Add docz

--- packages/default-web/src/components/atoms/Button/index.js
+++ packages/default-web/src/components/atoms/Button/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-export default function Button({ children, type = 'button', ...props }) {
-  return (
-    <button type={type || 'button'} {...props}>
-      {children}
-    </button>
-  );
-}
+export const StyledButton = styled.button`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 0.4rem;
+  font-size: 1.6rem;
+`;
+
+const Button = ({ children, ...props }) => <StyledButton {...props}>{children}</StyledButton>;
+
+export default Button;
 
 Button.propTypes = {
   children: PropTypes.node,
